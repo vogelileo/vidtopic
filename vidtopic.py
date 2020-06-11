05c4fea70c26dc50ba782d345e808028761bc037
vogelileo <57670210+vogelileo@users.noreply.github.com>
2020-06-11 19:27:02 +0200

    Add files via upload

---
+++ vidtopic.py
@@ -0,0 +1,45 @@
+# Importing all necessary libraries
+import cv2
+import os
+
+# Read the video from specified path
+cam = cv2.VideoCapture(
+    "C:/Users/Leo Vogel/Desktop/Atomprojekte/randomthings/Pictures from Video/y2mate.com - Boyfriend Tag mit Frau Bra (reingeschissen)_Mer_sk40rUA_1080p.mp4")
+
+try:
+
+    # creating a folder named data
+    if not os.path.exists('data'):
+        os.makedirs('data')
+
+# if not created then raise error
+except OSError:
+    print('Error: Creating directory of data')
+
+# frame
+currentframe = 0
+
+while(True):
+
+    # reading from frame
+    ret, frame = cam.read()
+
+    if ret:
+
+            # if video is still left continue creating images
+        name = './data/frame' + str(currentframe) + '.jpg'
+        if currentframe%30==0:
+            print('Creating...' + name)
+
+        # writing the extracted images
+        cv2.imwrite(name, frame)
+
+        # increasing counter so that it will
+        # show how many frames are created
+        currentframe += 1
+    else:
+        break
+
+# Release all space and windows once done
+cam.release()
+cv2.destroyAllWindows()
